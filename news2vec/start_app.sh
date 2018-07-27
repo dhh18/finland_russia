f0a0acd4a330786bf8c1be234d421c924bc871bb
arpol <arturs.polis@gmail.com>
2018-07-27 10:59:04 +0300

    added environment parameter

--- news2vec/start_app.sh
+++ news2vec/start_app.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+# Accepts one parameter:
+# development
+# or
+# production
 export FLASK_APP=news2vec
-export FLASK_ENV=development
+export FLASK_ENV=$1
 flask run --host=0.0.0.0 --port=8889
-
